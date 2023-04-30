73314519b844288b1adfd79d061d91345166a26e
Bilal Muzamil <bilal.muzamil.fabric.inc>
2023-04-30 14:55:20 +0500

    removing graphql

--- GraphQL/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    User: require("./User"),
-    Product: require("./Product"),
-    Inventory:require("./Inventory"),
-    Cart: require("./Cart")
-}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const app = require('./app')
+
+
+const port = 4000
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+
+//delete user
+app.get('/',async (req,res) => {
+
+    res.status(200).send({message: "hey there! Welcome to IOT-BASED-SHOPPING-CART"})
+
+})
